ba4cdb55fb495809f42011a472442a26595ab7db
S-e-r-g-e-i <s.ya.aronov.b@gmail.com>
2024-09-23 12:00:40 +0500

    Add files via upload

---
+++ 6mod_1task_v2.py
@@ -0,0 +1,55 @@
+#Зачем нужно наследование
+
+class Animal:
+    alive = True
+    fed = False
+
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        if food.edible is True:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+
+class Mammal(Animal):
+    ...
+
+
+class Predator(Animal):
+    ...
+
+
+class Paint:
+    edible = False
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Flower(Paint):
+    edible = False
+
+
+class Fruit(Paint):
+    edible = True
+
+# Проверка
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
